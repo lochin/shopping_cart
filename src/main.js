72c4a1738e5abb40555f74f690cba282c8bdf2df
Lochin <replytolochin@gmail.com>
2021-06-15 17:38:09 +0500

    first commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+import App from "./app/App.vue";
+import store from "./app/store";
+
+import router from "./app/router";
+
+createApp(App).use(router).use(store).mount("#app");
